6e62191caedd152f651561e3515cbd58cbf91538
wilson6532 <140465929+wilson6532@users.noreply.github.com>
2026-05-21 01:14:14 +0800

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String,List<String>> res  = new HashMap<>();
+
+        for(String s : strs){
+            char[] charArray  = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS  = new String(charArray);
+            res.putIfAbsent(sortedS,new ArrayList<String>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>(res.values());
+        
+    }
+}
