0cc4a5ae05080bf35ee5e05db763d159ac7dbf53
wilson6532 <140465929+wilson6532@users.noreply.github.com>
2026-05-27 11:31:43 +0800

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')','(');
+        closeToOpen.put(']','[');
+        closeToOpen.put('}','{');
+
+        for(char c :  s.toCharArray()){
+            if(closeToOpen.containsKey(c)){
+                if(!stack.isEmpty()&&stack.peek() == closeToOpen.get(c)){
+                    stack.pop();
+                } else{
+                    return false;
+                }
+            }else{
+                stack.push(c);
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
