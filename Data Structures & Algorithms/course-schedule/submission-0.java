314b9c85d08f399a16fa844386981ad9c6e9abe5
wilson6532 <140465929+wilson6532@users.noreply.github.com>
2026-06-11 13:46:29 +0800

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+      
+        for (int[] pre : prerequisites) {
+            int a = pre[0], b = pre[1];
+            adj.get(b).add(a);   
+            indegree[a]++;       
+        }
+
+   
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int finish = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();   
+            finish++;
+           
+            for (int next : adj.get(node)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.add(next);
+                }
+            }
+        }
+
+        return finish == numCourses;
+    }
+}
